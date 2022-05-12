15c97ce93ad5021284c8473ff428b4a57a1d1cba
Hasina <randrianantoaninahasina@gmail.com>
2022-05-12 11:35:18 +0300

    initialisation CRUD users

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const userRoutes = require('./routes/user.routes');
+require ('dotenv').config({ path: './config/.env' });
+require('./config/db');
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+
+
+//routes
+app.use('/api/user', userRoutes);
+
+
+//server
+app.listen(process.env.PORT, ()=> {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
